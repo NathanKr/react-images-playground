461becf1e0f4b9c54c32f69d7b5ead2bf1e5026a
Nathan Krasney <natankrasney@gmail.com>
2021-12-06 14:14:15 +0200

    react-icons

--- simple-app/src/App.js
+++ simple-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BsFillAlarmFill } from "react-icons/bs";
+import { FaGithub} from 'react-icons/fa'
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <header className="App-header">
         <h2>This is bootstrap icon using react-icons for better performnce</h2>
         <BsFillAlarmFill />
+        <h2>This is font awesome icon using react-icons for better performnce</h2>
+        <FaGithub />
         <h2>This image reside in another server</h2>
         <img
           src="https://media.istockphoto.com/photos/african-lion-headshot-looking-to-camera-picture-id689458958"
